064e465bbbf1bf54ec979f7a0fd3b21c320df9ac
Amir Hossain <amhossain14@winona.edu>
2017-10-05 20:08:50 -0500

    ...

--- CarModsTracker/AppDelegate.swift
+++ CarModsTracker/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 // i made this changes Amir Hossain
-
+// again
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
